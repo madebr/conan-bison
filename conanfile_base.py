edf6454e5f4fbbd85969e2373ac2dc0eddb6e67c
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-05 03:29:18 +0100

    cache autotools + split build/install

--- conanfile_base.py
+++ conanfile_base.py
@@ -1,5 +1,6 @@
-from conans.errors import ConanInvalidConfiguration
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans.errors import ConanException
+from contextlib import contextmanager
 import glob
 import os
 import shutil
@@ -15,8 +16,9 @@ class ConanFileBase(ConanFile):
     license = "GPL-3.0-or-later"
     exports = ["LICENSE.md"]
     exports_sources = ["patches/*.patch"]
-    _source_subfolder = "source_subfolder"
     requires = ("m4_installer/1.4.18@bincrafters/stable",)
+    _autotools = None
+    _source_subfolder = "source_subfolder"
 
     @property
     def _is_msvc(self):
@@ -40,14 +42,12 @@ class ConanFileBase(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
-    def build(self):
-        for filename in sorted(glob.glob("patches/*.patch")):
-            self.output.info('applying patch "%s"' % filename)
-            tools.patch(base_path=self._source_subfolder, patch_file=filename)
+    @contextmanager
+    def _build_context(self):
         with tools.vcvars(self.settings) if self._is_msvc else tools.no_op():
-            self._build_configure()
+            yield
 
-    def _build_configure(self):
+    def _configure_autotools(self):
         true = tools.which("true") or "/bin/true"
         true = tools.unix_path(true) if tools.os_info.is_windows else true
         args = ["HELP2MAN=%s" % true, "MAKEINFO=%s" % true, "--disable-nls"]
@@ -62,34 +62,60 @@ class ConanFileBase(ConanFile):
                 host = "i686-w64-mingw32"
             elif self.settings.arch == "x86_64":
                 host = "x86_64-w64-mingw32"
-            args.extend(['CC=$PWD/build-aux/compile cl -nologo',
-                         'CFLAGS=-%s' % self.settings.compiler.runtime,
+            args.extend(['CC={}/build-aux/compile cl -nologo'.format(tools.unix_path(self._source_subfolder)),
+                         'CXX={}/build-aux/compile cl -nologo'.format(tools.unix_path(self._source_subfolder)),
+                         'CFLAGS=-{}'.format(self.settings.compiler.runtime),
                          'LD=link',
                          'NM=dumpbin -symbols',
                          'STRIP=:',
-                         'AR=$PWD/build-aux/ar-lib lib',
+                         'AR={}/build-aux/ar-lib lib'.format(tools.unix_path(self._source_subfolder)),
                          'RANLIB=:',
                          "gl_cv_func_printf_directive_n=no"])
+        if self._autotools:
+            return self._autotools
+        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        self._autotools.configure(args=args, build=build, host=host, configure_dir=self._source_subfolder)
+        return self._autotools
 
-        env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        with tools.chdir(self._source_subfolder):
-            tools.replace_in_file("Makefile.in",
-                                  "dist_man_MANS = $(top_srcdir)/doc/bison.1",
-                                  "dist_man_MANS =")
-            tools.replace_in_file(os.path.join("src", "yacc.in"),
-                                  "@prefix@",
-                                  "${}_ROOT".format(self.name.upper()))
-            tools.replace_in_file(os.path.join("src", "yacc.in"),
-                                  "@bindir@",
-                                  "${}_ROOT/bin".format(self.name.upper()))
+    def build(self):
+        for filename in sorted(glob.glob("patches/*.patch")):
+            self.output.info('applying patch "%s"' % filename)
+            tools.patch(base_path=self._source_subfolder, patch_file=filename)
+        tools.replace_in_file(os.path.join(self._source_subfolder, "Makefile.in"),
+                              "dist_man_MANS = $(top_srcdir)/doc/bison.1",
+                              "dist_man_MANS =")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "yacc.in"),
+                              "@prefix@",
+                              "${}_ROOT".format(self.name.upper()))
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "yacc.in"),
+                              "@bindir@",
+                              "${}_ROOT/bin".format(self.name.upper()))
 
-            env_build.configure(args=args, build=build, host=host)
-            env_build.make()
-            env_build.install()
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make()
 
+    @property
+    def _os(self):
+        try:
+            return self.settings.os
+        except ConanException:
+            return self.settings.os_build
+
+    def package(self):
+        self.copy(pattern="COPYING", src=self._source_subfolder, dst="licenses")
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
         if self._is_msvc:
             shutil.move(os.path.join(self.package_folder, "lib", "liby.a"),
                         os.path.join(self.package_folder, "lib", "y.lib"))
+        if self._os == "Windows":
+            for root, _, files in os.walk(os.path.join(self.package_folder, "share")):
+                for file in files:
+                    if not file.endswith(".m4"):
+                        continue
+                    fn = os.path.join(root, file)
+                    contents = open(fn, "rb").read()
+                    open(fn, "wb").write(contents.replace(b"\n", b"\r\n"))
 
-    def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
